fccc6aa2a306dcd9ee4aefaf29a52f497e91eee6
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-04-28 23:55:49 +0300

    fixed betty styles for 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,22 +2,29 @@
 #include <stdlib.h>
 #include "sort.h"
 
+/**
+ * bubble_sort - Sorts an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+
 void bubble_sort(int *array, size_t size)
 {
-    int i, j, temp;
+	int i, j, temp;
 
-    for (j = size - 1; j > 0; j--)
-    {
-        for (i = 0; i <= j; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                temp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+	for (j = size - 1; j > 0; j--)
+	{
+		for (i = 0; i <= j; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
 
