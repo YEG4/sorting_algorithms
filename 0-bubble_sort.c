e9cb3b3bbfb598c5edbac2a376bbde26ca241cef
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-04-29 12:04:10 +0300

    edited 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,6 +13,10 @@ void bubble_sort(int *array, size_t size)
 {
 	int i, j, temp;
 
+	if (array == NULL || size <= 0)
+	{
+		return;
+	}
 	for (j = size - 1; j > 0; j--)
 	{
 		for (i = 0; i <= j; i++)
